550c33890d5d73709ddd8cc619871bf81a013422
fehmm <fefemendesfmm@gmail.com>
2025-08-20 12:28:32 -0300

    JAVA

---
+++ calculadora/Calculadora.java
@@ -0,0 +1,15 @@
+package calculadora;
+
+public class Calculadora {
+    
+
+    public Double numeroA;
+    public Double numeroB;
+
+    public void somar(){
+        Double resultadoSoma = this.numeroA + this.numeroB;
+        System.out.println(this.numeroA + " + " + this.numeroB + " + " + resultadoSoma);
+
+
+    }
+}
---
+++ olamundo.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+class OlaMundo {
+
+   public static void main(String[] args){
+       System.out.println("ola,mundo");
+       JOptionPane.showMessageDialog(null, "ola,mundo");
+       System.exit(0);
+       
+        
+       
+   }
+
+
+}	
