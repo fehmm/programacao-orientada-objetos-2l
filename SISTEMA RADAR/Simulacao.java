d0d0db6af43378f83cac0997a11151a7d7626fe8
fehmm <fefemendesfmm@gamil.com>
2025-09-08 21:15:52 -0300

    mantendo

--- SISTEMA RADAR/Simulacao.java
+++ SISTEMA RADAR/Simulacao.java
@@ -1,38 +1,37 @@
 
 public class Simulacao {
-
     public static void main(String[] args) {
         Carro opala = new Carro();
-        opala.velociade = 0;
-        opala.placa = "acl200";
-        opala.modelo = "comodro";
+        opala.velocidade = 0;
+        opala.placa = "ACLR300";
+        opala.modelo = "Comodoro";
         opala.ano = 76;
 
-        Radar Radar = new Radar();
+        Radar radar = new Radar();
         radar.modelo = "pardal";
-        radar.localizacao = "pistao sul";
+        radar.limiteVelocidade = 60;
+        radar.localizacao = "Pistão Sul";
 
+        opala.acelerar(); // 5
+        opala.acelerar(); // 10
+        opala.acelerar(); // 15
+        opala.acelerar(); 
         opala.acelerar();
         opala.acelerar();
+        opala.acelerar(); 
         opala.acelerar();
         opala.acelerar();
+        opala.acelerar(); 
         opala.acelerar();
-        opala.acelerar();
-        opala.acelerar();
-        opala.acelerar();
-        opala.acelerar();
-        opala.acelerar();
-        opala.acelerar();
-        opala.acelerar();
+        opala.acelerar(); // 60
 
-        radar.avaliarVelociade(opala);
+        radar.avaliarVelocidade(opala);
 
-        opala.acelerar();
-        radar.avaliarVelociade(opala);
-        
-        
+        opala.acelerar(); // 65
+        radar.avaliarVelocidade(opala);
 
+        opala.frear(); // 60
+        radar.avaliarVelocidade(opala);
 
     }
-    
 }
